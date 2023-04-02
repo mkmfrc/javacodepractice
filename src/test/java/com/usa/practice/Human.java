15d51edf80bf6fb895d6bb44fa4af96ac05b7e38
mkmfrc <mkmfrc@gmail.com>
2023-04-02 03:28:05 -0400

    All rights reserved to MKMFRC

---
+++ src/test/java/com/usa/practice/Human.java
@@ -0,0 +1,29 @@
+package com.usa.practice;
+
+public class Human {
+	
+	//this is a main method
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+System.out.println("hello java");
+		
+// how to declare the data
+//1.  need to understand the VALUE
+//2. Data Type
+//3. variable
+
+
+	int initialDeposit = 1500;
+	int wid = 50;
+	int deposit = 200;
+	int totalBalance = initialDeposit - wid + deposit;
+	
+	System.out.println(totalBalance);
+			
+	}
+
+}
+
+
---
+++ src/test/java/com/usa/practice/Method_Types.java
@@ -0,0 +1,32 @@
+package com.usa.practice;
+
+public class Method_Types {
+	
+	//this is void method
+	public void getEmployees(){
+	}
+
+	//this is return method
+	public int getSalary(){
+		return 0;
+	}
+
+	public String getName(){
+		return null;
+	}
+	
+	//Parameter method which returns arguments
+	public void getEmployeeInfo(int salary, int bonus){
+	}
+	
+	//this is static void method
+		static public void getEmployees1(){
+		}
+		
+		
+		public static void main(String[] args) {
+			
+		}
+		
+		
+}
